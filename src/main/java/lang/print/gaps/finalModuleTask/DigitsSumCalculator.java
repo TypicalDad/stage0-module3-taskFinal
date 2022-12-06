87d45fff4cda55acf584e82b1dde607b9f611e34
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-06 12:24:03 +0300

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int Thousands = number /1000;
+        int afterThousands = number - Thousands*1000;
+        int Hundreds = afterThousands /100;
+        int afterHundreds = afterThousands - Hundreds*100;
+        int tenth = afterHundreds /10;
+        int aftertenth = afterHundreds - tenth*10;
+        int x = Thousands + Hundreds + tenth + aftertenth;
+        System.out.println(x);
     }
 }
