6b814ac6710f8aaccd3208090e01f150d0347cec
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-06 11:04:09 +0300

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int newNymber = number;
+        int newNumber = newNymber /100;
+        int nowNumber = number - (newNumber *100);
+        int blueNumber = nowNumber / 10;
+        int newwNumber = nowNumber - (blueNumber *10);
+        System.out.println(newwNumber +""+ blueNumber +""+ newNumber);
     }
 }
